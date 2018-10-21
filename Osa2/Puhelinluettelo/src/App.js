402d6aa00a8941935fc85e2c502a92fb8fb8652c
jussilah <heli.jussila@cgi.com>
2018-10-21 16:31:40 +0300

    2.11 done.

--- Osa2/Puhelinluettelo/src/App.js
+++ Osa2/Puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import Filter from './components/Filter'
 import Data from './components/Data'
 
@@ -6,18 +7,24 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            persons: [
-                { name: 'Arto Hellas', number: '040-123456' },
-                { name: 'Martti Tienari', number: '040-123456' },
-                { name: 'Arto Järvinen', number: '040-111111' },
-                { name: 'Lea Kutvonen', number: '040-123456' }
-            ],
+            persons: [],
             newName: '',
             newNumber: '',
             filter: ''
 
         }
     }
+
+    componentDidMount() {
+        console.log('did mount')
+        axios
+          .get('http://localhost:3001/persons')
+          .then(response => {
+            console.log('promise fulfilled')
+            this.setState({ persons: response.data })
+          })
+      }
+    
 
     addName = (event) => {
         event.preventDefault()
