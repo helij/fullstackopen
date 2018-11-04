64ce5514bdd1089c32c32cd18717ffe6b9202b7a
helij <heli.h.jussila@gmail.com>
2018-11-04 07:51:40 +0200

    Fixed problems

---
+++ Osa4/blogilista/controllers/users.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+const formatUser = (user) => {
+  return {
+    id: user.id,
+    username: user.username,
+    name: user.name,
+    adult: user.adult
+  }
+}
+
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const body = request.body
+
+    const existingUser = await User.find({ username: body.username })
+    if (existingUser.length>0) {
+      return response.status(400).json({ error: 'username must be unique' })
+    }
+
+    if (body.password.length < 3) {
+      return response.status(400).json({ error: 'password lenght has to be bigger than three' })
+    }
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+      adult: body.adult === undefined ? true : body.adult
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+  } catch (exception) {
+    console.log(exception)
+    response.status(500).json({ error: 'something went wrong...' })
+  }
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(formatUser))
+})
+
+module.exports = usersRouter
--- Osa4/blogilista/models/user.js
+++ Osa4/blogilista/models/user.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-
-mongoose.connect(config.mongoUrl)
 
 const User = mongoose.model('User', {
   username: String,
