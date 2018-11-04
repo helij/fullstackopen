2a5a7b8e70030fe96faccb7ed3b5b1d0af4d3e22
helij <heli.h.jussila@gmail.com>
2018-11-04 06:47:14 +0200

    4.15 done

--- Osa4/blogilista/controllers/users.js
+++ Osa4/blogilista/controllers/users.js
@@ -2,6 +2,16 @@ const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+const formatUser = (user) => {
+  return {
+    id: user.id,
+    username: user.username,
+    name: user.name,
+    adult: user.adult
+  }
+}
+
+
 usersRouter.post('/', async (request, response) => {
   try {
     const body = request.body
@@ -23,6 +33,11 @@ usersRouter.post('/', async (request, response) => {
     console.log(exception)
     response.status(500).json({ error: 'something went wrong...' })
   }
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users.map(formatUser))
 })
 
 module.exports = usersRouter
