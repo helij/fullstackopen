b746546253dfec7b27987cec9d100e08c8532cf2
jussilah <heli.jussila@cgi.com>
2018-11-16 09:27:32 +0200

    5.1 done

---
+++ Osa5/bloglist-frontend/src/App.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blogs: [],
+      newBlog: '',
+      showAll: true,
+      error: null,
+      username: '',
+      password: '',
+      user: null
+    }
+  }
+
+  componentDidMount() {
+    blogService.getAll().then(blogs =>
+      this.setState({ blogs })
+    )
+  } 
+
+  login = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+  
+      this.setState({ username: '', password: '', user})
+    } catch(exception) {
+      this.setState({
+        error: 'käyttäjätunnus tai salasana virheellinen',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+
+    const loginForm = () => (
+      <div>
+        <h2>Kirjaudu</h2>
+
+        <form onSubmit={this.login}>
+          <div>
+            käyttäjätunnus
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            salasana
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button>kirjaudu</button>
+        </form>
+      </div>
+    )
+
+
+    const blogForm = () => (
+      <div>
+        {this.state.blogs.map(blog => 
+          <Blog key={blog._id} blog={blog}/>
+        )}
+      </div>
+    )
+
+    
+    return (
+      <div>
+
+        {this.state.user === null ?
+          loginForm() :
+          <div>
+            <h2>blogs</h2>
+            <p>{this.state.user.name} logged in</p>
+            {blogForm()}
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
