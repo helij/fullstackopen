6321a3985cada29e9be239e5adb194baebf1aec1
helij <heli.h.jussila@gmail.com>
2018-11-21 12:15:41 +0200

    6.3 done

--- Osa6/redux-anecdotes-v2/src/index.js
+++ Osa6/redux-anecdotes-v2/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import store from './store'
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 
+import App from './App';
+import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer
+})
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 const render = () => {
   ReactDOM.render(
