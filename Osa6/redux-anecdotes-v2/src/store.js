5767dac1ccaf8fff1daf9d814870168dd8327c5f
helij <heli.h.jussila@gmail.com>
2018-11-22 07:08:17 +0200

    6.13 done

--- Osa6/redux-anecdotes-v2/src/store.js
+++ Osa6/redux-anecdotes-v2/src/store.js
@@ -1,6 +1,22 @@
-import { createStore } from 'redux'
-import reducer from './reducers/anecdoteReducer'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+import notificationReducer from './reducers/notificationReducer'
 
-const store = createStore(reducer)
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer,
+  filter: filterReducer
+})
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
 
 export default store
